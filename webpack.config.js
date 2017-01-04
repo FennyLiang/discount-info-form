946d0fa6ed2ebac329d820e45e7d3b74649e7e7a
FennyLiang <shras1993@gmail.com>
2017-01-04 15:29:53 +0800

    add fetch dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var combineLoaders = require('webpack-combine-loaders');
 
 module.exports = {
-  entry: './index.jsx',
+  entry: ['babel-polyfill','./index.jsx'],
   output: {
     filename: './bundle.js',
 
@@ -57,6 +57,6 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    })
+    }),
   ],
 }
